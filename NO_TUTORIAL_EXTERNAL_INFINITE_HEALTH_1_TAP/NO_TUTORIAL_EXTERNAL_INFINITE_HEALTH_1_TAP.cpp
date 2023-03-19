6f00a3823fdcf806f6859f9b5185d1db9fee9f4f
Vinqueire <Frolof192@gmail.com>
2023-03-19 20:23:49 +0100

    I managed to do an external fully without a referencing the internet, im so happy ^^

--- NO_TUTORIAL_EXTERNAL_INFINITE_HEALTH_1_TAP/NO_TUTORIAL_EXTERNAL_INFINITE_HEALTH_1_TAP.cpp
+++ NO_TUTORIAL_EXTERNAL_INFINITE_HEALTH_1_TAP/NO_TUTORIAL_EXTERNAL_INFINITE_HEALTH_1_TAP.cpp
@@ -1,49 +1,117 @@
 #include <iostream>
 #include <Windows.h>
 #include <TlHelp32.h>
-#include <Psapi.h>
+#include <vector>
 
-DWORD getProcessId(const wchar_t* processName);
+DWORD getProcId(const wchar_t* procName);
+uintptr_t getModuleBaseAddress(DWORD procId, const wchar_t* modName);
+uintptr_t findDMAAddy(HANDLE hproc, uintptr_t ptr, std::vector<unsigned int> offsets);
 
 int main()
 {
-	DWORD procId = getProcessId(L"ac_client.exe");
-	std::cout << procId << std::endl; // This is here just so the code compiles lol
+	DWORD procId = getProcId(L"ac_client.exe");
+	std::cout << "procId is: " << procId << std::endl;
+	uintptr_t baseAddr = getModuleBaseAddress(procId, L"ac_client.exe");
+	std::cout << "Base adress is: 0x" << std::hex << baseAddr << std::dec << std::endl;
+
+	HANDLE hproc = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
+	std::vector<unsigned int> healthOffset = { 0xF8 };
+	uintptr_t dynAddr = baseAddr + 0x10F4F4;
+	std::cout << "dynamicAddr is: 0x" << std::hex << dynAddr << std::dec << std::endl;
+
+	uintptr_t healthAddress = findDMAAddy(hproc, dynAddr, healthOffset);
+
+	int currentHealth = 0;
+	ReadProcessMemory(hproc, (BYTE*)healthAddress, &currentHealth, sizeof(currentHealth), 0);
+	std::cout << "Current health is: " << currentHealth << std::endl;
+
+	int updatedHealth = 777;
+	WriteProcessMemory(hproc, (BYTE*)healthAddress, &updatedHealth, sizeof(updatedHealth), 0);
+	std::cout << "Now your health is: " << updatedHealth << std::endl;
+
 
 	system("pause");
 	return 0;
 }
 
-DWORD getProcessId(const wchar_t* processName)
+DWORD getProcId(const wchar_t* procName)
 {
-	PROCESSENTRY32 pe32;
 	DWORD procId = 0;
 	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-	if (snapshot == INVALID_HANDLE_VALUE || snapshot == 0)
+	if (snapshot == INVALID_HANDLE_VALUE)
 	{
-		std::cout << "Invalid handle value";
+		std::cout << "getProcId snapshot has invalid value" << std::endl;
 		return 1;
 	}
-	pe32.dwSize = sizeof(PROCESSENTRY32);
 
-	if (Process32First(snapshot, &pe32))
+	PROCESSENTRY32 entry;
+	entry.dwSize = sizeof(entry);
+
+	if (Process32First(snapshot, &entry))
 	{
 		do
 		{
-			if (!_wcsicmp(pe32.szExeFile, processName))
+			if (!_wcsicmp(entry.szExeFile, procName))
 			{
-				procId = pe32.th32ProcessID;
-				std::cout << "ProcId is: " << procId << std::endl;
+				procId = entry.th32ProcessID;
 				break;
 			}
-		} while (Process32Next(snapshot, &pe32));
+		} 
+		while (Process32Next(snapshot, &entry));
 	}
+	CloseHandle(snapshot);
 	if (procId == 0)
 	{
-		std::cout << "No process assigned" << std::endl;
+		std::cout << "getProcId procId has value of 0, failed to get procid\n";
+		return 1;
+	}
+	return procId;
+}
+
+uintptr_t getModuleBaseAddress(DWORD procId, const wchar_t* modName)
+{
+	uintptr_t modAddr = 0;
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+
+	if (snapshot == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "getModuleBaseAddress snapshot has invalid value\n";
+		return 1;
 	}
 
+	MODULEENTRY32 entry;
+	entry.dwSize = sizeof(entry);
+
+	if (Module32First(snapshot, &entry))
+	{
+		do
+		{
+			if (!_wcsicmp(entry.szModule, modName))
+			{
+				modAddr = (uintptr_t)entry.modBaseAddr;
+				break;
+			}
+		} 
+		while (Module32Next(snapshot, &entry));
+	}
 	CloseHandle(snapshot);
-	return procId;
+	if (modAddr == 0)
+	{
+		std::cout << "getModuleBaseAddress modAddr is 0, failed to get modBaseAddr\n";
+		return 1;
+	}
+	return modAddr;
+}
+
+uintptr_t findDMAAddy(HANDLE hproc, uintptr_t ptr, std::vector<unsigned int> offsets)
+{
+	uintptr_t addr = ptr;
+
+	for (unsigned int i = 0; i < offsets.size(); ++i)
+	{
+		ReadProcessMemory(hproc, (BYTE*)addr, &addr, sizeof(addr), 0);
+		addr += offsets[i];
+	}
+	return addr;
 }
